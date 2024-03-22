ab8ff98cec24ba0daaf715d69929a8cf9f83b2d6
Renatuscape <rrjonsberg@gmail.com>
2024-03-22 11:06:34 +0100

    Made sure placeholder name is displayed if string is empty. Cleaned up comments.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,24 +4,17 @@ export default function Player() {
   const playerName = useRef(); // always a javascript object with a current property that contains the stored value
 
   const [enteredPlayerName, setEnteredPlayerName] = useState('');
-  // const [submitted, setSubmitted] = useState(false);
 
-  // function handleChange(event){
-  //   setSubmitted(false);
-  //   setEnteredPlayerName(event.target.value);
-  // }
-
-  function handleClick(){
+  function handleClick() {
     setEnteredPlayerName(playerName.current.value); // using ref lets you forego the handleChange that updates with every keystroke.
+    playerName.current.value = ''; // changes the DOM directly, violating the idea that React should handle all DOM interactions. Questionable practice, but useful here.
   }
 
   return (
     <section id="player">
-      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
+      <h2>Welcome {enteredPlayerName !== '' ? enteredPlayerName : 'unknown entity'}</h2>
       <p>
         <input ref={playerName} type="text"
-        // onChange={handleChange}
-        // value={enteredPlayerName}
         />
         <button onClick={handleClick}>Set Name</button>
       </p>
