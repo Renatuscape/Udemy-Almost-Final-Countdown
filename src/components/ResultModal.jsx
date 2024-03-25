385ed486929884be393efa90bb1dc003b2e02512
Renatuscape <rrjonsberg@gmail.com>
2024-03-25 19:07:20 +0100

    added onReset property.

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,8 +1,12 @@
 import { forwardRef, useImperativeHandle, useRef } from 'react';
 
 //forward ref must be wrapped around the component in order to allow ref forwarding. Ref is passed as a second parameter with forward Ref
-const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) { // a modal is a dialogue opened on top of the screen
-const dialog = useRef();
+const ResultModal = forwardRef(function ResultModal(
+    { targetTime, remainingTime, onReset }, ref) { // a modal is a dialogue opened on top of the screen
+    const dialog = useRef();
+
+    const userLost = remainingTime <= 0;
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
     useImperativeHandle(ref, () => { //useImperativeHandle allows creating custom methods that are called by the parent component. It improves readability, as opposed to calling showModal in the parent component.
         return {
@@ -13,10 +17,10 @@ const dialog = useRef();
     });
 
     return (<dialog ref={dialog} className="result-modal">
-        <h2>You {result}</h2>
+        {userLost && <h2>You lost</h2>}
         <p>The target time was <strong>{targetTime} seconds.</strong></p>
-        <p>You stopped the timer with <strong>X seconds left.</strong></p>
-        <form method="dialog">
+        <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
+        <form method="dialog" onSubmit={onReset}>
             {/* form with dialogue containing a button will automatically close the dialogue. */}
             <button>Close</button>
         </form>
