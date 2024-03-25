4d0d655fd2b0836c4629eb003e1df99c0e08dcdb
Renatuscape <rrjonsberg@gmail.com>
2024-03-25 14:48:35 +0100

    added useImperativeHandle for clarity

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,8 +1,18 @@
-import { forwardRef } from 'react';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
 
 //forward ref must be wrapped around the component in order to allow ref forwarding. Ref is passed as a second parameter with forward Ref
 const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) { // a modal is a dialogue opened on top of the screen
-    return (<dialog ref={ref} className="result-modal">
+const dialog = useRef();
+
+    useImperativeHandle(ref, () => { //useImperativeHandle allows creating custom methods that are called by the parent component. It improves readability, as opposed to calling showModal in the parent component.
+        return {
+            open() {
+                dialog.current.showModal(); // opens the modal dialogue. Open can be used as a prop on dialog, but calling it programmatically gives a dark backdrop
+            }
+        };
+    });
+
+    return (<dialog ref={dialog} className="result-modal">
         <h2>You {result}</h2>
         <p>The target time was <strong>{targetTime} seconds.</strong></p>
         <p>You stopped the timer with <strong>X seconds left.</strong></p>
